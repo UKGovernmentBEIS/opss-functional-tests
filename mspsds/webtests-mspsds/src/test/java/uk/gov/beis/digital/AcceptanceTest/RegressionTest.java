deb75381fcbea1274f16a5a572a1a271c62ae5fa
Tom Bell <tom.bell@softwire.com>
2019-05-30 16:53:16 +0100

    Fix whitespace formatting in regression test classes

--- mspsds/webtests-mspsds/src/test/java/uk/gov/beis/digital/AcceptanceTest/RegressionTest.java
+++ mspsds/webtests-mspsds/src/test/java/uk/gov/beis/digital/AcceptanceTest/RegressionTest.java
@@ -6,11 +6,11 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(format = { "html:target/cucumber-reports/html",
-		"json:target/cucumber-reports/cucumber.json" }, features = {
-				"src/test/resources/Features" }, glue="uk.gov.beis.digital.stepdefs",
-	 tags = {"@regression","~@ignore,@Pending"})
-
+@CucumberOptions(
+	format = { "html:target/cucumber-reports/html", "json:target/cucumber-reports/cucumber.json" },
+	features = { "src/test/resources/Features" },
+	tags = { "@regression","~@ignore,@Pending" },
+	glue="uk.gov.beis.digital.stepdefs"
+)
 public class RegressionTest {
-
 }
