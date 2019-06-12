d9a2065e5fcced7c03ba8bc1b986d789bca51b0d
nasirkhanbeis <nasirkhan.beis@gmail.com>
2019-06-12 09:07:35 +0100

    standard nano-material tests

--- cosmetics/cosmetics-web-tests/src/test/java/uk/gov/beis/cosmetics/stepdefs/WhenSteps.java
+++ cosmetics/cosmetics-web-tests/src/test/java/uk/gov/beis/cosmetics/stepdefs/WhenSteps.java
@@ -7,89 +7,107 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import uk.gov.beis.cosmetics.pagemodel.AddProductPage;
 import uk.gov.beis.cosmetics.pagemodel.LoginPage;
+import uk.gov.beis.cosmetics.pagemodel.NanoMaterialPage;
 
 public class WhenSteps extends SharedWebDriver {
 
 	private WebDriver driver;
 	private LoginPage loginPage;
 	private AddProductPage addProductPage;
+	private NanoMaterialPage nanoMaterialPage;
 
 	public WhenSteps(SharedWebDriver driver) {
 		this.driver = driver;
 		loginPage = PageFactory.initElements(driver, LoginPage.class);
 		addProductPage = PageFactory.initElements(driver, AddProductPage.class);
+		nanoMaterialPage = PageFactory.initElements(driver, NanoMaterialPage.class);
 	}
 
 	@When("^I enter internal reference number \"(.*?)\"$")
 	public void i_enter_internal_reference_number(String arg1) throws Throwable {
 		addProductPage.enter_reference_number(arg1);
 	}
-	
+
 	@When("^I select physical form as \"(.*?)\"$")
 	public void i_select_physical_form_as(String arg1) throws Throwable {
-	    addProductPage.select_radio_button_by_text(arg1);
-	    addProductPage.click_continue();
+		addProductPage.select_radio_button_by_text(arg1);
+		addProductPage.click_continue();
 	}
 
 	@When("^I click on continue$")
 	public void i_click_on_continue() throws Throwable {
 		Thread.sleep(2000);
-	    addProductPage.click_continue();
+		addProductPage.click_continue();
 	}
 
 	@When("^I should see manual-journey step \"(.*?)\"$")
 	public void i_should_see_manual_journey_step(String arg1) throws Throwable {
-	   addProductPage.verify_cosmetics_page_headers(arg1);
+		addProductPage.verify_cosmetics_page_headers(arg1);
 	}
 
 	@When("^I notify product manually$")
 	public void i_notify_product_manually() throws Throwable {
-		  loginPage.click_by_text("Add cosmetic products");
-		  addProductPage.select_radio_button_by_text("No, they have not been notified in the EU");
-		  addProductPage.click_continue();
-	      addProductPage.select_radio_button_by_text("No");
-	      addProductPage.click_continue();
-	      addProductPage.enter_productname("TestManual-Prod");
-	      addProductPage.enter_reference_number("TestRef-2003");
-	      addProductPage.select_radio_button_by_text("Yes");
-		  addProductPage.click_continue();
-	      addProductPage.enter_country_imported_from("India");
-	      addProductPage.click_continue();
-	      addProductPage.select_radio_button_by_text("The cosmetic product is a single item");
-	      addProductPage.click_continue();
-	      addProductPage.select_radio_button_by_text("No");
-	      addProductPage.click_continue();
-	      addProductPage.select_radio_button_by_text("Loose powder");
-	      addProductPage.click_continue();
-	      addProductPage.select_radio_button_by_text("No");
-	      addProductPage.click_continue();
-	      addProductPage.select_radio_button_by_text("No");
-	      addProductPage.click_continue();
-	      addProductPage.select_prod_category("Skin care products - Hand care products");
-	      addProductPage.select_radio_button_by_text("Choose a predefined frame formulation");
-	      addProductPage.click_continue();
+		loginPage.click_by_text("Add cosmetic products");
+		addProductPage.select_radio_button_by_text("No, they have not been notified in the EU");
+		addProductPage.click_continue();
+		addProductPage.select_radio_button_by_text("No");
+		addProductPage.click_continue();
+		addProductPage.enter_productname("TestManual-Prod");
+		addProductPage.enter_reference_number("TestRef-2003");
+		addProductPage.select_radio_button_by_text("Yes");
+		addProductPage.click_continue();
+		addProductPage.enter_country_imported_from("India");
+		addProductPage.click_continue();
+		addProductPage.select_radio_button_by_text("The cosmetic product is a single item");
+		addProductPage.click_continue();
+		addProductPage.select_radio_button_by_text("No");
+		addProductPage.click_continue();
+		addProductPage.select_radio_button_by_text("Loose powder");
+		addProductPage.click_continue();
+		addProductPage.select_radio_button_by_text("No");
+		addProductPage.click_continue();
+		addProductPage.select_radio_button_by_text("No");
+		addProductPage.click_continue();
+		addProductPage.select_prod_category("Skin care products - Hand care products");
+		addProductPage.select_radio_button_by_text("Choose a predefined frame formulation");
+		addProductPage.click_continue();
 	}
 
 	@When("^I choose frame formulation$")
 	public void i_choose_frame_formulation() throws Throwable {
 		addProductPage.select_frame_formulation("Skin Care Cream, Lotion, Gel");
-	    addProductPage.click_continue();
+		addProductPage.click_continue();
 	}
-	
 
 	@Then("^I should be asked if the product contains anti-dandruff agents$")
-	public void i_should_be_asked_if_the_product_contains_anti_dandruff_agents() throws Throwable {   
+	public void i_should_be_asked_if_the_product_contains_anti_dandruff_agents() throws Throwable {
 	}
-	
+
 	@When("^I select notify product manually$")
 	public void i_select_notify_product_manually() throws Throwable {
-		addProductPage.add_product_manually();
-	    
+		addProductPage.notify_manually();
+
 	}
 
 	@When("^I select Yes it contains nano material$")
 	public void i_select_Yes_it_contains_nano_material() throws Throwable {
 		addProductPage.select_radio_button_by_text("Yes");
-	    
+
+	}
+
+	@When("^I select user likely to be exposed by \"(.*?)\"$")
+	public void i_select_user_likely_to_be_exposed_by(String arg1) throws Throwable {
+		nanoMaterialPage.choose_expose_to(arg1);
+
+	}
+
+	@When("^I click on \"(.*?)\" on annex$")
+	public void i_click_on_on_annex(String arg1) throws Throwable {
+		nanoMaterialPage.select_radio_button_by_text(arg1);
+	}
+
+	@Then("^I should see multi selectable exposure route \"(.*?)\"$")
+	public void i_should_see_multi_selectable_exposure_route(String arg1) throws Throwable {
+
 	}
 }
--- cosmetics/cosmetics-web-tests/src/test/resources/Features/Functional/StandardNanoMaterialTests.feature
+++ cosmetics/cosmetics-web-tests/src/test/resources/Features/Functional/StandardNanoMaterialTests.feature
@@ -16,19 +16,60 @@ Scenario: Verify how user is exposed to nano-material question
 When I click "Rinse off"
 Then I should see "How is the user likely to be exposed to the nanomaterials?"
 
+@nano-material
+Scenario: Verify correct Annex displayed as per the inci purpose
+When I select user likely to be exposed by "Dermal"
+And I select user likely to be exposed by "Oral"
+And I click on continue
+Then I should see "List the nanomaterials in the cosmetic product"
+
+@nano-material
 Scenario: Verify nano material exposure is given in multi selectable checkboxes
 Given I am on exposure route page
 Then I should see multi selectable exposure route "Dermal"
-And I should see multi selectable exposure route "Dermal"
-And I should see multi selectable exposure route "Dermal"
+And I should see multi selectable exposure route "Oral"
+And I should see multi selectable exposure route "Inhalation"
 
-Scenario: Verify nano material inci to display legislation for each inci purpose type- Passed
+@nano-material
+Scenario: Enter Inci name
+Given I enter INCI name "test"
+And I click on continue
+Then I should see "What is the purpose of test?"
 
-Scenario: Verify when inci purpose is colorant then Annex 4 legislation question is triggered- Passed
+@nano-material
+Scenario: Annex 4 is displayed when colorant is choosen
+Given I select purpose for nanomaterial "Colorant"
+And I click on continue
+Then I should see "Is test listed in EC regulation 1223/2009, Annex 4?"
 
-Scenario: Verify when INCI purpose is preservative then Annex 5 legislation is triggered - Passed
+@nano-material
+Scenario: Annex 5 is displayed when preservative is choosen
+Given I click back
+And I select purpose for nanomaterial "Preservative"
+And I click on continue
+Then I should see "Is test listed in EC regulation 1223/2009, Annex 5?"
 
-Scenario: Verify when INCI purpose is UV filter then Annex 6 legislation is triggered - Passed
+@nano-material
+Scenario: Annex 6 is displayed when UV Filter  is choosen
+Given I click back
+And I select purpose for nanomaterial "UV filter"
+And I click on continue
+Then I should see "Is test listed in EC regulation 1223/2009, Annex 6?"
+
+@nano-material
+Scenario: Verify nano-material is used in accordnance with the restiction for each selected purpose
+Given I click back
+And I select purpose for nanomaterial "Preservative"
+And I click on continue
+When I click on "Yes" on annex 
+And I click on continue
+Then I should see "Is it used in accordance with the restrictions for that material?"
+
+@nano-material
+Scenario: Verify nano-material journey page is complete once restriction page is confirmed
+When I click "Yes"
+And I click on continue
+Then I should see "What category of cosmetic product is it?"
 
 
 
