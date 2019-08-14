b0692ce4788548bf7e8bf2b2650c5e9def2056f9
nasirkhanbeis <nasirkhan.beis@gmail.com>
2019-08-14 14:04:30 +0100

    updated sharedwebdriver

--- mspsds/webtests-mspsds/src/test/java/uk/gov/beis/digital/stepdefs/SharedWebDriver.java
+++ mspsds/webtests-mspsds/src/test/java/uk/gov/beis/digital/stepdefs/SharedWebDriver.java
@@ -100,7 +100,14 @@ public class SharedWebDriver extends EventFiringWebDriver {
 			 * To run tests in chrome
 			 */
 			else if (browser.equalsIgnoreCase("chrome")) {
+				if(platform.equalsIgnoreCase("local"))
+				{
+				 System.setProperty("webdriver.chrome.driver", 
+						 System.getProperty("user.dir") + "/src/test/resources/chromedriver");
+				}
+				else {
 				System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+				}
 
 				ChromeOptions options = new ChromeOptions();
 				options.setHeadless(true);
