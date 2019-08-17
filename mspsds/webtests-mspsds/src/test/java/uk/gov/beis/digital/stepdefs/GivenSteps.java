cd112d183dbe856848320b08c809efd553b0d6d4
nasirkhanbeis <nasirkhan.beis@gmail.com>
2019-08-18 00:15:36 +0100

    updated local run settings

--- mspsds/webtests-mspsds/src/test/java/uk/gov/beis/digital/stepdefs/GivenSteps.java
+++ mspsds/webtests-mspsds/src/test/java/uk/gov/beis/digital/stepdefs/GivenSteps.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.support.PageFactory;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
+import uk.gov.beis.digital.mspsds.Utils.AppProperties;
 import uk.gov.beis.digital.mspsds.Utils.EnvironmentProperties;
 import uk.gov.beis.digital.mspsds.pagemodel.AssigneePage;
 import uk.gov.beis.digital.mspsds.pagemodel.LoginPage;
@@ -12,7 +13,7 @@ public class GivenSteps {
 
 	private LoginPage loginPage;
 	private AssigneePage assigneePage;
-
+	private String platform=AppProperties.get("platform");
 	public GivenSteps(SharedWebDriver driver) {
 		loginPage = PageFactory.initElements(driver, LoginPage.class);
 		assigneePage = PageFactory.initElements(driver, AssigneePage.class);
@@ -20,7 +21,12 @@ public class GivenSteps {
 
 	@Given("^I login as OPSS user$")
 	public void i_login_as_OPSS_user() throws Throwable {
-		loginPage.launch_app(EnvironmentProperties.getServiceUrl());
+		if (platform.equals("local")) {
+			loginPage.launch_app(AppProperties.get("envurl"));
+		} else {
+			loginPage.launch_app(EnvironmentProperties.getServiceUrl());
+
+		}
 		Thread.sleep(5000);
 		
 		loginPage.login_as_opss();
@@ -30,7 +36,12 @@ public class GivenSteps {
 
 	@Given("^I login as Trading standard user$")
 	public void i_login_as_Trading_standard_user() throws Throwable {
-		loginPage.launch_app(EnvironmentProperties.getServiceUrl());
+		if (platform.equals("local")) {
+			loginPage.launch_app(AppProperties.get("envurl"));
+		} else {
+			loginPage.launch_app(EnvironmentProperties.getServiceUrl());
+
+		}
 		Thread.sleep(5000);
 
 		loginPage.login_as_ts();
